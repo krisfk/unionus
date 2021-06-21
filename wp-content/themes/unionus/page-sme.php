deb4a1c01afcc775ec6c18ea64476b327d23a7a7
kaiyu li <krisfk@gmail.com>
2021-06-22 04:32:38 +0800

    .

--- wp-content/themes/unionus/page-sme.php
+++ wp-content/themes/unionus/page-sme.php
@@ -288,9 +288,31 @@ get_header();
                     <td>
                         職員聯絡閣下的時間:
                         <select class="form-select" aria-label="是否支援whatsapp">
-                            <option value="" disabled selected>是否支援whatsapp？</option>
-                            <option value="是">是</option>
-                            <option value="否">否</option>
+                            <option value=""></option>
+                            <option value="1:00pm-2:00pm">1:00pm-2:00pm</option>
+                            <option value="2:00pm-3:00pm">2:00pm-3:00pm</option>
+                            <option value="3:00pm-4:00pm">3:00pm-4:00pm</option>
+                            <option value="4:00pm-5:00pm">4:00pm-5:00pm</option>
+                            <option value="5:00pm-6:00pm">5:00pm-6:00pm</option>
+                            <option value="6:00pm-7:00pm">6:00pm-7:00pm</option>
+                            <option value="7:00pm-8:00pm">7:00pm-8:00pm</option>
+                            <option value="8:00pm-9:00pm">8:00pm-9:00pm</option>
+                            <option value="9:00pm-10:00pm">9:00pm-10:00pm</option>
+                            <option value="10:00pm-11:00pm">10:00pm-11:00pm</option>
+                            <option value="11:00pm-12:00pm">11:00pm-12:00pm</option>
+                            <option value="12:00pm-1:00am">12:00pm-1:00am</option>
+                            <option value="1:00am-2:00am">1:00am-2:00am</option>
+                            <option value="2:00am-3:00am">2:00am-3:00am</option>
+                            <option value="3:00am-4:00am">3:00am-4:00am</option>
+                            <option value="4:00am-5:00am">4:00am-5:00am</option>
+                            <option value="5:00am-6:00am">5:00am-6:00am</option>
+                            <option value="6:00am-7:00am">6:00am-7:00am</option>
+                            <option value="7:00am-8:00am">7:00am-8:00am</option>
+                            <option value="8:00am-9:00am">8:00am-9:00am</option>
+                            <option value="9:00am-10:00am">9:00am-10:00am</option>
+                            <option value="10:00am-11:00am">10:00am-11:00am</option>
+                            <option value="11:00am-12:00am">11:00am-12:00am</option>
+                            <option value="12:00am-1:00pm">12:00am-1:00pm</option>
                         </select>
                     </td>
                 </tr>
