4c8a99781822ea38640c4d3f31b8c66ff2093990
kaiyu li <krisfk@gmail.com>
2021-06-22 03:42:26 +0800

    .

--- wp-content/themes/unionus/page-sme.php
+++ wp-content/themes/unionus/page-sme.php
@@ -179,7 +179,7 @@ get_header();
                     <h2 class="gold mt-5 text-center">我們的服務
                     </h2>
 
-                    <div class="row">
+                    <div class="row mt-4">
                         <div class="col-4 text-center">
                             <img class="sme-icon"
                                 src="<?php echo get_template_directory_uri();?>/assets/images/sme-icon-1.png" alt="">
