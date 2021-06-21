a36350128915231b3c33ee2cfbccd73af5b5130f
kaiyu li <krisfk@gmail.com>
2021-06-22 06:51:46 +0800

    .

--- wp-content/themes/unionus/page-under-construction.php
+++ wp-content/themes/unionus/page-under-construction.php
@@ -1,1 +1,18 @@
-under construction
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/javascript">
+    window.location = "https://unionus.com.hk/sme/";
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
