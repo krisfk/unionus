cd1f5a66f98e517f856095e182b6b766387ff3f9
kaiyu li <krisfk@gmail.com>
2021-06-24 05:26:05 +0800

    .

--- wp-content/themes/unionus/page-improve-tu-plan.php
+++ wp-content/themes/unionus/page-improve-tu-plan.php
@@ -87,7 +87,7 @@ get_header();
 
 
 
-        <div class="inner-container2">
+        <div class="inner-container2 mx-auto">
             <div class="text-center mt-7">
 
                 <div class="little-gold-bar mx-auto"></div>
