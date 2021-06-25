58d80832ce4e647ac707fa46b1927db97531ca1f
kaiyu li <krisfk@gmail.com>
2021-06-25 23:19:55 +0800

    .

--- wp-content/themes/unionus/page-our-team.php
+++ wp-content/themes/unionus/page-our-team.php
@@ -40,6 +40,14 @@ get_header();
         </div>
 
 
+        <div class="text-center mt-7">
+
+            <div class="little-gold-bar mx-auto"></div>
+            <h2 class="gold mt-5">我們的團隊</h2>
+        </div>
+
+
+
 
 
         <div class="row gx-5 justify-content-center mt-6">
