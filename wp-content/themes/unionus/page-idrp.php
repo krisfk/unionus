9b3ad0945edeb617fa1db3f58d1620b3ca9f46d4
kaiyu li <krisfk@gmail.com>
2021-06-26 04:23:23 +0800

    .

---
+++ wp-content/themes/unionus/page-idrp.php
@@ -0,0 +1,325 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+
+
+
+<section class="sec1 pt-const2">
+
+    <div class="container">
+
+        <div class="text-center">
+            <h1 class="mx-auto gold">什麼是IDRP（牽頭式債務舒緩）？</h1>
+        </div>
+
+        <div class="sep mx-auto mt-3"> <img class="sep-flo"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/sep-flo.png" alt=""></div>
+
+        <div class="inner-container mx-auto mt-5 text-center">
+            <span class="big-size">IDRP</span>
+            牽頭式債務舒緩是一種債務重組解決方案，其功能與“個人自願安排”和“債務重組計劃”相似。該計劃為擁有多個債權人的債務人提供協助；
+            債務人只需要與最大債權人談判並制定新的還款計劃.(一般情況下以銀行作為主要債權人)
+        </div>
+
+
+        <div class="text-center mt-7">
+
+            <div class="little-gold-bar mx-auto"></div>
+            <h2 class="gold mt-5">我們的團隊</h2>
+        </div>
+
+
+        <div class="row gx-5 inner-container2 mx-auto">
+            <div class="col-6 staff-col">
+                <h2 class="gold mt-3">Joseph </h2>
+                <div class="little-gold-bar mx-auto mt-3 mb-3"></div>
+                <div class="gold big-size">Partner/CPA (Practising) <br>
+                    香港會計師公會成員 <br>
+                    債務重組專員
+
+                </div>
+                <div class="mt-3">吳先生過往一直為各大上市集團及私人企業提供會計和審核的工作，並為各階層就業人士提供報稅,審計和債務重組整合服務，憑藉過人的待客之道和熱誠在過往數年內處裡超過千宗成功個案。
+                </div>
+            </div>
+
+
+
+            <div class="col-6 staff-col">
+                <h2 class="gold mt-3">Midas </h2>
+                <div class="little-gold-bar mx-auto mt-3 mb-3"></div>
+                <div class="gold big-size">Partner
+                    <br>
+                    市場營運總監
+                    <br>
+                    資深債務重組策劃
+
+                </div>
+                <div class="mt-3">
+                    創辦人之一的馬先生在過往數年內致力為各職業人士提供專業並有效的債務重組解決方案,根據每位客戶的疑問和困難提供獨立意見和作出分析，處理債務重組成功個案具備相當經驗，並獲得多次客戶推薦。
+
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="mt-6">
+            <div><img class="flo-icon" src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/flo-icon.png"
+                    alt=""></div>
+
+            <div class="row inner-container2 justify-content-center mx-auto">
+                <div class="col-12 staff-col">
+                    <h2 class="gold mt-4">Alex Cheung
+                    </h2>
+                    <div class="little-gold-bar mx-auto mt-3 mb-3"></div>
+                    <div class="gold big-size">Finance Mannger
+                        <br>
+
+                        MPF and Banking
+
+
+                    </div>
+                    <div class="mt-3">
+                        提供有關保險,強積金有關專業意見， <br>並為各大中小企代辦公司開戶。
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+        <div class="mt-6">
+            <div><img class="flo-icon" src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/flo-icon.png"
+                    alt=""></div>
+
+            <div class="row inner-container2 justify-content-center mx-auto">
+                <div class="col-12 staff-col">
+                    <h2 class="gold mt-4">Solicitor
+                    </h2>
+                    <div class="little-gold-bar mx-auto mt-3 mb-3"></div>
+                    <div class="gold big-size">
+                        LLB法律學士
+
+
+                    </div>
+                    <div class="mt-3">
+                        在法律事務有豐富的經驗並為註冊律師,同時擔任爾聯Unionus的榮譽法律顧問，並與客戶和其他相關專業人士建立了廣泛的網絡。他經常受個人和企業客戶委託處理破產與IVA個人自願安排，住宅，工業和商業房地產有關的各種法律服務，包括
+                        :
+                    </div>
+                    <div class="mt-3">
+                        <ul class="gold d-inline-block text-start">
+                            <li>破產</li>
+                            <li>債務重組 DRP</li>
+                            <li>債務舒緩 IVA</li>
+                            <li>土地購置和處置</li>
+                            <li>樓宇管理</li>
+                            <li>可行性研究</li>
+                            <li>關於出售和購買房地產的融資</li>
+                            <li>修訂房屋批地</li>
+
+                        </ul>
+
+                        <ul class="gold d-inline-block  text-start">
+
+                            <li>私人土地招標文件和代表</li>
+                            <li>處理租賃、租約和牌照</li>
+                            <li>續訂和延長租賃、租約和牌照</li>
+                            <li>處理按揭、貸款文件和押記</li>
+                            <li>擁有權利執行</li>
+                            <li>項目發展</li>
+                            <li>買賣協議</li>
+                            <li>處理以禮物轉讓房地產的文件及建議</li>
+
+                        </ul>
+
+
+                    </div>
+
+                    <div class="mt-3">此外，林律師還擁有（a）一般商業和企業諮詢工作;及（b）處理本地及海外客戶稅務、信託及遺囑的經驗，包括：
+                    </div>
+
+                    <div class="mt-3">
+                        <ul class="gold d-inline-block  text-start">
+                            <li>遺產管理及分配</li>
+                            <li>遺產規劃</li>
+                            <li>房地產授產安排</li>
+                            <li>遺囑認證和遺產管理書</li>
+                            <li>信託安排和酌情信託</li>
+                            <li>遺囑起草</li>
+                        </ul>
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+        <div class="mt-6">
+            <div><img class="flo-icon" src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/flo-icon.png"
+                    alt=""></div>
+
+            <div class="row inner-container2 justify-content-center mx-auto">
+                <div class="col-12 staff-col">
+                    <h2 class="gold mt-4">Gary Li
+
+                    </h2>
+                    <div class="little-gold-bar mx-auto mt-3 mb-3"></div>
+                    <div class="gold big-size">Master of Laws, Master of Accounting, MMed.Sc., M.Sc
+
+                        <br>
+
+                        法律碩士,會計學碩士,項目管理理學碩士 <br>
+                        榮譽法律顧問
+
+
+                    </div>
+                    <div class="mt-3">
+                        在李律師曾在2000年至2007年出任執業大律師,並由2009年成為執業律師至今，現任Unionus爾聯的法律顧問，豐富的經驗令李律師得以完善解決客戶目前面對的問題。
+
+
+
+
+                    </div>
+                    <div class="gold mt-4 big-size">主要服務項目：</div>
+
+                    <div class="mt-3 d-flex justify-content-center">
+                        <ul class="gold d-inline-block text-start">
+                            <li>民事和刑事訴訟</li>
+                            <li>破產、IVA、DRP</li>
+                            <li>公司清盤</li>
+                            <li>仲裁和調解</li>
+                            <li>股東糾紛</li>
+                            <li>貨物買賣糾紛</li>
+                            <li>強制土地和土地糾紛</li>
+
+
+
+                        </ul>
+
+                        <ul class="gold d-inline-block  text-start">
+
+                            <li>禁令申請</li>
+                            <li>運輸</li>
+                            <li>家庭信託</li>
+                            <li>人身傷害案件</li>
+                            <li>租賃和樓宇管理事宜</li>
+                            <li>起草各種業務協議，包括股份購買協議</li>
+
+
+                        </ul>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</section>
+
+
+<!-- </div> -->
+
+
+<style type="text/css">
+/* .bg-shape-1 {
+    display: none;
+
+} */
+
+.bg-shape-2 {
+    display: none;
+
+}
+</style>
+
+
+<script type="text/javascript">
+const skrollrObj = skrollr.init();
+</script>
+
+<script type="text/javascript">
+$(function() {
+
+
+    $('.gold-table-div').height($('.gold-table-div-wrapper-col').height() - 100);
+
+    // .gold-table-div.silver
+    // $('.gold-table-div.silver').height($('.gold-table-div-wrapper-col').height() - 100);
+    // $('.home-banner-div').width($(window).width());
+    // $('.home-banner-div').height($(window).height());
+
+
+    // $('.company-intro-content-1').css({
+    //     'top': $('.company-intro-row-div').height() / 2 - $('.company-intro-content-1').height() - 30 +
+    //         'px'
+    // })
+
+    // $('.company-intro-content-2').css({
+    //     'bottom': $('.company-intro-row-div').height() / 2 - $('.company-intro-content-2').height() -
+    //         30 +
+    //         'px'
+    // })
+
+
+
+
+});
+</script>
+<?php
+
+get_footer();
