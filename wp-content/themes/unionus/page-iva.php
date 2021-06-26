936efc38574ca2d7337b4f087965991a53e69699
kaiyu li <krisfk@gmail.com>
2021-06-27 05:05:09 +0800

    .

--- wp-content/themes/unionus/page-iva.php
+++ wp-content/themes/unionus/page-iva.php
@@ -272,34 +272,12 @@ get_header();
         <div class="inner-container2 mx-auto">
 
             <div class="mt-5">
-                <div class="gold big-size">．債務重組計劃（DRP）</div>
-                債務重組計劃比IVA和IDRP更簡單。這是一個相對較快的解決方案。它不需要債務人參與任何法律程序。我們在債務人同意的情況下制定還款計劃，並與債權人進行談判。DRP允許債權人直接與債務人聯繫。
+                <div class="gold big-size">1.申請IVA的過程需時多久？</div>
+                一般來說，由簽署文件，宣誓，排期上庭直到開債權人會議，約需三個月時間，全程會由具我們會計師與律師跟進，保障閣下的私隱。
             </div>
 
 
-            <div class="mt-5">
-                <div class="gold big-size">．個人自願安排（IVA）</div>
-                IVA的債務重組程序相對較長，其中包括法律程序，並在執業律師前宣誓。　 <br> <br>
-                並且，債務人需要任命一名被會計師或專業人士代為處理.
-            </div>
 
-
-            <div class="mt-5">
-                <div class="gold big-size">．DRP債務減免計劃</div>
-
-                債務重組計劃更簡單相對較快的解決方案 <br>
-                不需要債務人參與任何法律程序。
-
-            </div>
-
-
-            <div class="mt-5">
-                <div class="gold big-size">．IVA個人自願安排</div>
-
-                IVA的債務重組程序相對較長 <br>
-                債務人需要任命一名被會計師或專業人士代為處理
-
-            </div>
 
         </div>
 
