d752886427b40178baa85ad2e7b4683eda58bf0d
kaiyu li <krisfk@gmail.com>
2021-06-26 22:13:45 +0800

    .

--- wp-content/themes/unionus/page-drp.php
+++ wp-content/themes/unionus/page-drp.php
@@ -179,7 +179,9 @@ get_header();
         <div class="mt-5 drp-case-div">
             <ul>
                 <li class=" wow fadeInUp animated" style="opacity: 1; visibility: visible; animation-name: fadeInUp;">
-                    <div class="name">職業：Admin　月入：$18,400 </div>
+                    <div class="name">
+                        <h2 class="gold">職業：Admin　月入：$18,400</h2>
+                    </div>
                     <div class="nr">
 
                         <div class="gold-table-div">
