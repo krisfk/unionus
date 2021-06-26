ac47329c3158c6d9341943f42d335927653db97c
kaiyu li <krisfk@gmail.com>
2021-06-26 22:39:53 +0800

    .

--- wp-content/themes/unionus/page-drp.php
+++ wp-content/themes/unionus/page-drp.php
@@ -267,7 +267,9 @@ get_header();
                     </div>
                 </li>
                 <li class=" wow fadeInUp animated" style="opacity: 1; visibility: visible; animation-name: fadeInUp;">
-                    <div class="name">職業：Retail sale　月入：$17,000 </div>
+                    <div class="name">
+                        <h3 class="gold"> 職業：Retail sale　月入：$17,000</h3>
+                    </div>
                     <div class="nr">
                         <table cellspacing="0px">
                             <tbody>
