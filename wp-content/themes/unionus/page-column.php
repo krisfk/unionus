e1616f93ea2a0679fd2b3e2fc31af3ae241edbae
kaiyu li <krisfk@gmail.com>
2021-06-29 03:30:23 +0800

    .

--- wp-content/themes/unionus/page-column.php
+++ wp-content/themes/unionus/page-column.php
@@ -43,10 +43,10 @@ get_header();
             <div class="news-container">
 
                 <div class="news">
-                    <div class="w-50 d-inline-block float-start"><img class="w-100"
+                    <div class="w-50 d-inline-block float-start news-col"><img class="w-100"
                             src="https://unionus.com.hk/wp-content/uploads/2021/06/newspaper.png" alt="">
                     </div>
-                    <div class="w-50 text-start  d-inline-block float-start">
+                    <div class="w-50 text-start  d-inline-block float-start news-col">
                         <h3 class="title gold">
                             【破產流程】申請個人破產｜提交破產呈請｜破產的4個影響</h3>
 
