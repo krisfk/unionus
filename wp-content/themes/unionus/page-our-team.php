722ed77ea7f98faf0eff97d9f58b5d2e88909c06
kaiyu li <krisfk@gmail.com>
2021-06-29 02:18:03 +0800

    .

--- wp-content/themes/unionus/page-our-team.php
+++ wp-content/themes/unionus/page-our-team.php
@@ -50,8 +50,8 @@ get_header();
 
 </section>
 <div>
-    f
-    <img src="<?php echo get_template_directory_uri();?>/assets/images/team-img.png" alt="">
+
+    <img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/team-img.png" alt="">
 </div>
 <section>
     <div class="container">
