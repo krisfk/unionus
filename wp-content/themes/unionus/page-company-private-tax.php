be9fba0e7d047beec4aae09e99c80fc6f1ecdc68
kaiyu li <krisfk@gmail.com>
2021-06-30 23:55:48 +0800

    .

--- wp-content/themes/unionus/page-company-private-tax.php
+++ wp-content/themes/unionus/page-company-private-tax.php
@@ -26,16 +26,24 @@ get_header();
     <div class="container">
 
         <div class="text-center">
-            <h1 class="mx-auto gold">公司報稅</h1>
+            <h1 class="mx-auto gold">
+                <?php echo get_field('content_part_0');?>
+                <!-- 公司報稅 -->
+            </h1>
         </div>
 
         <div class="sep mx-auto mt-3"> <img class="sep-flo"
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/sep-flo.png" alt=""></div>
+                src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/sep-flo.png" alt=""></div>
 
-        <div class="inner-container mx-auto mt-5 text-center">任何公司都需要申報稅務，但無限公司一般只需要在個人報稅表上申報，有限公司則需要審計核數並遞交獨立報稅表。
+        <div class="inner-container mx-auto mt-5 text-center">
+            <?php echo get_field('content_part_1');?>
+            <!-- 任何公司都需要申報稅務，但無限公司一般只需要在個人報稅表上申報，有限公司則需要審計核數並遞交獨立報稅表。 -->
         </div>
         <div class="inner-container2 mx-auto">
-            <h2 class="gold mt-6">有限公司首次報稅注意事項</h2>
+
+            <?php echo get_field('content_part_2');?>
+
+            <!-- <h2 class="gold mt-6">有限公司首次報稅注意事項</h2>
 
             <div class="mt-4">
                 首次報稅： <br>
@@ -47,8 +55,11 @@ get_header();
                 2) 由開業至自訂結帳日<br>
                 例如1/7/2020 至31/12/2020 或 1/7/2020 至31/1/2021<br>
                 (即以上例子下年度稅期為 1/1/2021至31/12/2021 或 1/2/2021-31/1/2022)
-            </div>
+            </div> -->
 
+
+            <?php echo get_field('content_part_3');?>
+            <!-- 
 
             <h2 class="gold mt-5 text-end">其後報稅</h2>
 
@@ -57,7 +68,7 @@ get_header();
                 通常於四至五月會收到，需要發出日一個月內遞交。 <br>
                 但我們作為稅務代表可以為客戶提交延期申請，根據公司結帳日期有以下分別:
 
-            </div>
+            </div> -->
 
         </div>
 
@@ -67,11 +78,15 @@ get_header();
             <div class="gold-table-div-wrapper">
 
                 <img class="table-head-th-img"
-                    src="<?php echo get_template_directory_uri(); ?>/assets/images/table-head-th.png" alt="">
+                    src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-head-th.png" alt="">
 
 
                 <div class="gold-table-div">
-                    <table class="gold-table">
+
+                    <?php echo get_field('content_part_4');?>
+
+
+                    <!-- <table class="gold-table">
 
 
                         <tbody>
@@ -107,14 +122,18 @@ get_header();
 
                         </tbody>
 
-                    </table>
+                    </table> -->
                 </div>
 
             </div>
 
-            <div class="text-start mt-4 inner-container2 mx-auto">*此日期參考2020/21年度，每年安排或會有不同，須以稅務局刊登為準 <br>
+            <div class="text-start mt-4 inner-container2 mx-auto">
 
-                另外首年檔案號碼中字首的”23”亦會更改為相應行業類別的編號，例如05(進出口)、13(印刷)、22 (覆查個案) 等等
+                <?php echo get_field('content_part_5');?>
+
+                <!-- *此日期參考2020/21年度，每年安排或會有不同，須以稅務局刊登為準 <br>
+
+                另外首年檔案號碼中字首的”23”亦會更改為相應行業類別的編號，例如05(進出口)、13(印刷)、22 (覆查個案) 等等 -->
 
             </div>
         </div>
@@ -122,26 +141,40 @@ get_header();
         <div class="text-center mt-7">
 
             <div class="little-gold-bar mx-auto"></div>
-            <h2 class="gold mt-5">報稅需要提交什麼？</h2>
+            <h2 class="gold mt-5">
+                <?php echo get_field('content_part_6');?>
+
+                <!-- 報稅需要提交什麼？ -->
+            </h2>
         </div>
 
 
         <div class="row mt-6 gx-5">
 
             <div class="col-4 text-center">
-                <img class="tax-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/tax-icon-1.png"
-                    alt="">
+
+                <?php echo get_field('content_part_7');?>
+
+
+                <!-- <img class="tax-icon"
+                    src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/tax-icon-1.png" alt="">
+
+
 
                 <h3 class="gold mt-3">完全沒有營運</h3>
 
                 <div class="mt-4">收入為0或宣布公司處於不活動狀態
                     仍需要填寫報稅表，但可豁免審計。除了填寫相應的日期及股本外，稅表上只需要全填零。
-                </div>
+                </div> -->
 
 
             </div>
-            <div class="col-4 text-center"> <img class="tax-icon"
-                    src="<?php echo get_template_directory_uri(); ?>/assets/images/tax-icon-2.png" alt="">
+            <div class="col-4 text-center">
+                <?php echo get_field('content_part_8');?>
+
+                <!-- 
+                <img class="tax-icon"
+                    src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/tax-icon-2.png" alt="">
 
                 <h3 class="gold mt-3">營業額 HK$1 - $2,000,000 <br>
                     加其他小型法團定義*</h3>
@@ -151,28 +184,38 @@ get_header();
                     <a class="normal" href="https://www.ird.gov.hk/chi/tax/taxrep_ptr.htm"
                         target="_blank">https://www.ird.gov.hk/chi/tax/taxrep_ptr.htm</a>
 
-                </div>
+                </div> -->
 
             </div>
-            <div class="col-4 text-center"> <img class="tax-icon"
-                    src="<?php echo get_template_directory_uri(); ?>/assets/images/tax-icon-3.png" alt="">
+            <div class="col-4 text-center">
+
+                <?php echo get_field('content_part_9');?>
+
+                <!-- <img class="tax-icon"
+                    src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/tax-icon-3.png" alt="">
                 <h3 class="gold mt-3">營業額二百萬以上</h3>
                 <div class="mt-4">需要將稅表連同簽署後的審計報告及利得稅計算表遞交。
 
-                </div>
+                </div> -->
 
             </div>
         </div>
 
-        <div class="inner-container3 mx-auto mt-5">*如遺失了稅表，可以寫信給稅局補領，會寄至公司註冊地址 <br>
-            **因為審計及會計必須是一年帶一年的做，如遺失了以前的報稅及會計記錄，可去信稅局要求寄回先前的文件遞交記錄(需支付相關費用)
+        <div class="inner-container3 mx-auto mt-5">
+            <?php echo get_field('content_part_10');?>
+
+            <!-- *如遺失了稅表，可以寫信給稅局補領，會寄至公司註冊地址 <br>
+            **因為審計及會計必須是一年帶一年的做，如遺失了以前的報稅及會計記錄，可去信稅局要求寄回先前的文件遞交記錄(需支付相關費用) -->
         </div>
 
         <div class="text-center mt-7">
 
             <div class="little-gold-bar mx-auto"></div>
+            <?php echo get_field('content_part_11');?>
+            <!-- 
+
             <h2 class="gold mt-5">報稅後</h2>
-            <div class="mt-3">如果公司於稅務調整後財政為:</div>
+            <div class="mt-3">如果公司於稅務調整後財政為:</div> -->
         </div>
 
 
@@ -182,11 +225,13 @@ get_header();
                 <div class="gold-table-div-wrapper">
 
                     <img class="table-head-th-img"
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/table-head-th.png" alt="">
+                        src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-head-th.png" alt="">
 
 
                     <div class="gold-table-div silver">
-                        <table class="gold-table">
+                        <?php echo get_field('content_part_12');?>
+
+                        <!-- <table class="gold-table">
 
 
                             <tbody>
@@ -210,11 +255,11 @@ get_header();
 
                             </tbody>
 
-                        </table>
+                        </table> -->
                     </div>
 
                     <img class="table-foot-th-img"
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/table-foot.png" alt="">
+                        src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-foot.png" alt="">
 
 
 
@@ -224,11 +269,15 @@ get_header();
                 <div class="gold-table-div-wrapper">
 
                     <img class="table-head-th-img"
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/table-head-th.png" alt="">
+                        src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-head-th.png" alt="">
 
 
                     <div class="gold-table-div silver">
-                        <table class="gold-table">
+
+                        <?php echo get_field('content_part_13');?>
+
+
+                        <!-- <table class="gold-table">
 
 
                             <tbody>
@@ -254,11 +303,11 @@ get_header();
 
                             </tbody>
 
-                        </table>
+                        </table> -->
                     </div>
 
                     <img class="table-foot-th-img"
-                        src="<?php echo get_template_directory_uri(); ?>/assets/images/table-foot.png" alt="">
+                        src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-foot.png" alt="">
 
 
 
@@ -268,9 +317,11 @@ get_header();
 
 
         <div class="inner-container2 text-center mx-auto">
-            除本年度稅項外，大家每年也需要支付「暫繳稅」(即預付下一年的稅)。 <br>
+            <?php echo get_field('content_part_14');?>
+
+            <!-- 除本年度稅項外，大家每年也需要支付「暫繳稅」(即預付下一年的稅)。 <br>
             暫繳稅也可享用8.25%優惠稅率，但<span class="gold">不可</span>享用稅款寬減。 <br>
-            例如2020/21年度寬減稅款為100%及<span class="gold">上限</span>HK$10,000:
+            例如2020/21年度寬減稅款為100%及<span class="gold">上限</span>HK$10,000: -->
         </div>
 
 
@@ -279,11 +330,15 @@ get_header();
             <div class="gold-table-div-wrapper w-100">
 
                 <img class="table-head-th-img"
-                    src="<?php echo get_template_directory_uri(); ?>/assets/images/table-head-th.png" alt="">
+                    src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/table-head-th.png" alt="">
 
 
                 <div class="gold-table-div w-100">
-                    <table class="gold-table w-100">
+
+                    <?php echo get_field('content_part_15');?>
+
+
+                    <!-- <table class="gold-table w-100">
 
 
                         <tbody>
@@ -331,16 +386,20 @@ get_header();
 
                         </tbody>
 
-                    </table>
+                    </table> -->
                 </div>
 
             </div>
 
-            <div class="mt-4">下年如果寬免稅務後不需要交稅的話，稅局會退回款項。如果仍需要交稅的話亦會退回/補回 <br>
+            <div class="mt-4">
+                <?php echo get_field('content_part_16');?>
+
+                <!-- 
+                下年如果寬免稅務後不需要交稅的話，稅局會退回款項。如果仍需要交稅的話亦會退回/補回 <br>
 
                 *每年稅款寬減資料: <a class="normal"
                     href="https://www.gov.hk/tc/residents/taxes/taxfiling/taxrates/salariesrates.htm" target="_blank">
-                    https://www.gov.hk/tc/residents/taxes/taxfiling/taxrates/salariesrates.htm</a>
+                    https://www.gov.hk/tc/residents/taxes/taxfiling/taxrates/salariesrates.htm</a> -->
             </div>
         </div>
 
@@ -360,8 +419,12 @@ get_header();
             src="http://localhost:8888/unionus/wp-content/themes/unionus/assets/images/sep-flo.png" alt=""></div> -->
 
     <div class="sep mx-auto mt-const"> <img class="sep-flo"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/sep-flo.png" alt=""></div>
-    <table class="me-auto ms-auto mt-5">
+            src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/sep-flo.png" alt=""></div>
+
+    <?php echo get_field('content_part_17');?>
+
+
+    <!-- <table class="me-auto ms-auto mt-5">
         <tbody>
             <tr>
                 <td class="align-top gold-border-right pe-3">
@@ -383,7 +446,7 @@ get_header();
                 </td>
             </tr>
         </tbody>
-    </table>
+    </table> -->
     <div class="sep2 mx-auto mt-5">
     </div>
 
