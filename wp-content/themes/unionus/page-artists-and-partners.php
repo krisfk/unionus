57d44c60468653d56d480f74505980b19fd3eb1d
kaiyu li <krisfk@gmail.com>
2021-07-01 15:12:25 +0800

    .

--- wp-content/themes/unionus/page-artists-and-partners.php
+++ wp-content/themes/unionus/page-artists-and-partners.php
@@ -28,7 +28,10 @@ get_header();
     <div class="container">
 
         <div class="text-center">
-            <h1 class="mx-auto gold">藝人與合作伙伴</h1>
+            <h1 class="mx-auto gold">
+                <?php echo get_field('content_part_0'); ?>
+                <!-- 藝人與合作伙伴 -->
+            </h1>
         </div>
 
         <div class="sep mx-auto mt-3"> <img class="sep-flo"
@@ -36,18 +39,29 @@ get_header();
 
         <div class="inner-container mx-auto mt-5 text-center">
 
-            <h2 class="gold">Master Mic</h2>
+            <h2 class="gold">
+                <?php echo get_field('content_part_1'); ?>
+
+                <!-- Master Mic -->
+            </h2>
 
             <div class="row mt-5 gx-5">
 
                 <div class="col-6">
-                    <img class="w-100"
+
+                    <?php echo get_field('content_part_2'); ?>
+
+
+                    <!-- <img class="w-100"
                         src="https://unionus.com.hk/wp-content/uploads/2021/06/1602677248_classify_img-e1624905361304.jpeg"
-                        alt="">
+                        alt=""> -->
                 </div>
                 <div class="col-6 text-start">
 
-                    <div>
+                    <?php echo get_field('content_part_3'); ?>
+
+
+                    <!-- <div>
                         <h3 class="gold">2005</h3>
                         自從2005年MASTAMIC開始了他的HIP HOP生涯後，他便一直是城中HIP HOP界的焦點，
                         作為香港HIP HOP界的先鋒，他擅長將富有社會性的歌詞和獨特的街頭色彩相互融合，
@@ -60,7 +74,17 @@ get_header();
                         MASTAMIC多次與本地及海外藝人和團體合作。<br>
                         包括本地RAPPER LO.J及多個HIP HOP藝人如農夫、歐陽靖、廿四味、DJ TOMMY、DJ GALAXY、INK、CRITICAL (瑞典)、<br>
                         C.O.U (北京)、DUMDUE (廣洲)。
-                    </div>
+                    </div> -->
+
+
+                </div>
+
+
+                <div class="col-6 text-start mt-5">
+
+                    <?php echo get_field('content_part_4'); ?>
+
+
                     <!-- <div class="mt-3">
                         除此之外，更如不同界別的歌手合作製作音樂，<br>
                         如流行歌手容祖兒、方皓玟、張繼聰、關楚耀、鍾達茵R&B歌手J.J LIN，獨立樂隊TONICK及獨立歌手萱寧。<br>
@@ -73,26 +97,14 @@ get_header();
                     </div> -->
                 </div>
 
-
-                <div class="col-6 text-start mt-5">
-                    <div class="mt-3">
-                        除此之外，更如不同界別的歌手合作製作音樂，<br>
-                        如流行歌手容祖兒、方皓玟、張繼聰、關楚耀、鍾達茵R&B歌手J.J LIN，獨立樂隊TONICK及獨立歌手萱寧。<br>
-                        其中和關楚耀合作單曲《佔領》最高更獲吒叱903專業推介冠軍位置。
-                    </div>
-                    <div class="mt-3">
-                        MASTAMIC仍有多次大型演出的經驗，<br>
-                        如側田紅館演唱會，CONCERT YY黃偉文作品展演唱會和鄭秀文合唱及本地樂隊野仔九展演唱會嘉賓。<br>
-                        其他包括陳冠希、 MC HOTDOG、RUBBERBAND、太極樂隊、及梁漢文，更獲邀到馬來西亞與台灣著名歌手曹格一同演出。
-                    </div>
-                </div>
-
                 <div class="col-6 position-relative mt-5">
 
-                    <iframe class="w-100 start-0 position-relative" src="https://www.youtube.com/embed/L_9JELdtjDQ"
+                    <?php echo get_field('content_part_5'); ?>
+
+                    <!-- <iframe class="w-100 start-0 position-relative" src="https://www.youtube.com/embed/L_9JELdtjDQ"
                         title="YouTube video player" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
+                        allowfullscreen></iframe> -->
 
                 </div>
             </div>
@@ -100,7 +112,11 @@ get_header();
             <div class="text-center mt-7">
 
                 <div class="little-gold-bar mx-auto"></div>
-                <h2 class="gold mt-5">合作伙伴
+
+                <?php echo get_field('content_part_6'); ?>
+
+
+                <!-- <h2 class="gold mt-5">合作伙伴
                 </h2>
 
                 <div class="mt-5">
@@ -108,7 +124,7 @@ get_header();
                             src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/logo.png" alt=""></a>
                     <a href="#" class="partner-logo-a"><img
                             src="https://unionus.com.hk/wp-content/themes/unionus/assets/images/logo.png" alt=""></a>
-                </div>
+                </div> -->
             </div>
 
 
