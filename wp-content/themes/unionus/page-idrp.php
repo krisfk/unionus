c8fc6cca24928e494a4402ddadeca535afb91d24
kaiyu li <krisfk@gmail.com>
2021-07-01 16:21:43 +0800

    .

--- wp-content/themes/unionus/page-idrp.php
+++ wp-content/themes/unionus/page-idrp.php
@@ -26,14 +26,20 @@ get_header();
     <div class="container">
 
         <div class="text-center">
-            <h1 class="mx-auto gold">什麼是IDRP（牽頭式債務舒緩）？</h1>
+            <h1 class="mx-auto gold">
+                <?php echo get_field('content_part_0');?>
+                <!-- 什麼是IDRP（牽頭式債務舒緩）？ -->
+            </h1>
         </div>
 
         <div class="inner-container2 mx-auto">
             <div class="sep mx-auto mt-3"> <img class="sep-flo"
                     src="<?php echo get_template_directory_uri(); ?>/assets/images/sep-flo.png" alt=""></div>
 
-            <div class="inner-container mx-auto mt-5 text-center">
+            <?php echo get_field('content_part_1');?>
+
+
+            <!-- <div class="inner-container mx-auto mt-5 text-center">
                 <span class="big-size">IDRP</span>
                 牽頭式債務舒緩是一種債務重組解決方案，其功能與“個人自願安排”和“債務重組計劃”相似。該計劃為擁有多個債權人的債務人提供協助；
                 債務人只需要與最大債權人談判並制定新的還款計劃.(一般情況下以銀行作為主要債權人)
@@ -48,11 +54,13 @@ get_header();
                 牽頭式債務舒緩，這是DRP的簡單版本。因為它不需要與單個債權人進行獨立談判. 我們的專家將根據債務人的還款能力製定還款計劃，該計劃包括每月還款，利率和期限。《牽頭式債務舒緩》是破產/ DRP/
                 IVA以外的債務處理解決方案，它不要求債務人按照IVA的要求進行任何法律程序。
 
-            </div>
+            </div> -->
 
             <div class="mt-5">
-                <h2 class="gold">IDRP的好處:</h2>
-                減輕利息負擔，綜合還款計劃並將根據債務人的還款能力來製定，以便債務人實現財務自由，節省費用，債務人不需要雇用律師，也不需要像IVA個人自願安排向法院提供請願。債務人可以通過IDRP的還款計劃節省債務繁複的法律手續。債務人可以在預期時間內統一債務並歸還有關欠款予債權人。
+                <?php echo get_field('content_part_2');?>
+
+                <!-- <h2 class="gold">IDRP的好處:</h2>
+                減輕利息負擔，綜合還款計劃並將根據債務人的還款能力來製定，以便債務人實現財務自由，節省費用，債務人不需要雇用律師，也不需要像IVA個人自願安排向法院提供請願。債務人可以通過IDRP的還款計劃節省債務繁複的法律手續。債務人可以在預期時間內統一債務並歸還有關欠款予債權人。 -->
 
             </div>
         </div>
@@ -61,7 +69,9 @@ get_header();
 
 
         <div class="mt-5 inner-container2 mx-auto">
-            <h2 class="gold">有關資格申請:</h2>
+            <?php echo get_field('content_part_3');?>
+
+            <!-- <h2 class="gold">有關資格申請:</h2>
 
             <div> 請人需要提交其所有個人信息作為評估，當中包括：
             </div>
@@ -72,10 +82,14 @@ get_header();
                 <li>收入證明</li>
                 <li>稅單（如有）</li>
 
-            </ul>
+            </ul> -->
 
             <div>
-                <div class="big-size"> ***注意***
+
+                <?php echo get_field('content_part_4');?>
+
+
+                <!-- <div class="big-size"> ***注意***
                 </div>
                 <div class="gold">不適用於二線財務機構的私人貸款計劃 如果您的資料合適IDRP還款計劃,我們的專員將會根據申請人的還款能力製定一個新的還款計劃。 <br>
                     包括：</div>
@@ -86,7 +100,7 @@ get_header();
                     <li>還款期</li>
                 </ul>
 
-                <div class="mt-3">一旦達成還款協議，將直接提交最大債權人徵得其同意。簽署協議，在雙方同意新的還款計劃的情況下。債務人按照其中接受的建議將固定利率的貸款償還給主要債權人。 </div>
+                <div class="mt-3">一旦達成還款協議，將直接提交最大債權人徵得其同意。簽署協議，在雙方同意新的還款計劃的情況下。債務人按照其中接受的建議將固定利率的貸款償還給主要債權人。 </div> -->
             </div>
 
 
@@ -97,62 +111,81 @@ get_header();
         <div class="text-center mt-7">
 
             <div class="little-gold-bar mx-auto"></div>
-            <h2 class="gold mt-5">IVA，DRP和IDRP有什麼區別？</h2>
+            <h2 class="gold mt-5">
+                <?php echo get_field('content_part_5');?>
+                <!-- IVA，DRP和IDRP有什麼區別？ -->
+            </h2>
         </div>
 
         <div class="text-center mt-5 inner-container2 mx-auto">
+            <?php echo get_field('content_part_6');?>
+            <!-- 
             與DRP/IVA債務舒緩/債務重組計劃相比，IDRP牽頭式債務舒緩的主要區別在於它只需要處理最大的債權人，而無需安排與其他債權人的個別談判，這在申請方面將更加簡單。
             <br> <br>
             <span class="gold">
                 而且，它不涉及任何法律程序。IDRP 比IVA和DRP更簡單，這是一個相對較快的解決方案。</span><br><br>
             它不需要債務人參與任何法律程序。在債務人同意的情況下制定還款計劃，並與債權人進行談判。IDRP允許債權人直接與債務人聯繫。一般情況下IVA的債務重組程序相對較長，其中包括法律程序，並在律師事務所宣誓。並且在過程中，債務人需要任命一名代名人,
-            該人仕必須持有專業資格。
+            該人仕必須持有專業資格。 -->
         </div>
 
         <div class="text-center mt-7">
 
             <div class="little-gold-bar mx-auto"></div>
-            <h2 class="gold mt-5">常見問題</h2>
+            <h2 class="gold mt-5">
+                <?php echo get_field('content_part_7');?>
+
+                <!-- 常見問題 -->
+            </h2>
         </div>
 
         <div class="inner-container2 mx-auto">
 
             <div class="mt-5">
-                <div class="big-size gold">1.我要支付多少服務費？</div>
+                <?php echo get_field('content_part_8');?>
+
+                <!-- <div class="big-size gold">1.我要支付多少服務費？</div>
                 <div>視乎個案複雜程度而定, 一般以客戶總節省利息計算，並已經包含在每月還款額內。
-                </div>
+                </div> -->
             </div>
 
             <div class="mt-5">
-                <div class="big-size gold">2.我的利息支出減少多少？</div>
+                <?php echo get_field('content_part_9');?>
+
+                <!-- <div class="big-size gold">2.我的利息支出減少多少？</div>
                 <div>通過牽頭式債務舒緩後的利息支付相對較低，一般可以節省70%利息以上。
 
-                </div>
+                </div> -->
             </div>
 
             <div class="mt-5">
-                <div class="big-size gold">3.整個過程需要多長時間？
+                <?php echo get_field('content_part_10');?>
+
+                <!-- <div class="big-size gold">3.整個過程需要多長時間？
                 </div>
                 <div>通常，處理時間為20-25個工作日。
 
-                </div>
+                </div> -->
             </div>
             <div class="mt-5">
-                <div class="big-size gold">4.是否會影響我的信用信譽？
+                <?php echo get_field('content_part_11');?>
+
+                <!-- <div class="big-size gold">4.是否會影響我的信用信譽？
                 </div>
                 <div>IDRP不會影響債務人的信用聲譽。
 
 
-                </div>
+                </div> -->
             </div>
             <div class="mt-5">
-                <div class="big-size gold">5.如果申請IDRP遭到拒絕，我還有其他選擇嗎？
+                <?php echo get_field('content_part_12');?>
+
+                <!-- <div class="big-size gold">5.如果申請IDRP遭到拒絕，我還有其他選擇嗎？
 
                 </div>
                 <div>並非所有的銀行和金融機構都接受IDRP。如果債權人不接受該計劃，則債務人可以轉向其他解決方案，例如IVA和DRP。
 
 
-                </div>
+                </div> -->
             </div>
 
 
