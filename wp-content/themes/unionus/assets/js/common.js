c3107dc34bba6f440908e7b53c432e0cbb26c3f4
kaiyu li <krisfk@gmail.com>
2021-07-11 05:08:42 +0800

    .

--- wp-content/themes/unionus/assets/js/common.js
+++ wp-content/themes/unionus/assets/js/common.js
@@ -18,7 +18,7 @@ $(function() {
             $('.top-menu-div').css({ height: '100%' });
         } else {
             $('.top-menu-div').css({ height: 'auto' });
-            $('.top-menu-div li').fadeIn(0);
+            $('.top-menu-div .top-menu li').fadeIn(0);
         }
     });
 
