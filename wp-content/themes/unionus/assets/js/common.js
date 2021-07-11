98a07a2a18fbd2faea5bcd0047432026c645a6c5
kaiyu li <krisfk@gmail.com>
2021-07-12 05:20:02 +0800

    .

--- wp-content/themes/unionus/assets/js/common.js
+++ wp-content/themes/unionus/assets/js/common.js
@@ -16,7 +16,7 @@ $(function() {
         opacity: 0,
     });
 
-    $('.sep').css({ width: '20%' });
+    $('.sep').css({ width: '20%', opacity: 0 });
 
     $(window).on(' scroll', function() {
         checkvisible();
@@ -71,7 +71,7 @@ $(function() {
                 !$('.sep').eq(i).hasClass('animate__animated')
             ) {
                 $('.sep').eq(i).addClass('animate__animated');
-                $('.sep').eq(i).animate({ width: '100%' }, 500);
+                $('.sep').eq(i).animate({ width: '100%', opacity: 1 }, 500);
 
                 // $('.sep').eq(i).addClass('animate__fadeIn');
                 // $('.sep').eq(i).addClass('delay-2');
