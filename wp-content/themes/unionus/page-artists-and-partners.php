163d26ca1f76379e3bd1f19e32d3db69c9f62ade
kaiyu li <krisfk@gmail.com>
2021-07-13 00:07:10 +0800

    .

--- wp-content/themes/unionus/page-artists-and-partners.php
+++ wp-content/themes/unionus/page-artists-and-partners.php
@@ -47,7 +47,7 @@ get_header();
 
             <div class="row mt-5 gx-5">
 
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12 fadeinleft-ele ">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12 fadeleft-ele ">
 
                     <?php echo get_field('content_part_2'); ?>
 
@@ -56,7 +56,7 @@ get_header();
                         src="https://unionus.com.hk/wp-content/uploads/2021/06/1602677248_classify_img-e1624905361304.jpeg"
                         alt=""> -->
                 </div>
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12 text-start  fadeinright-ele">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12 text-start  faderight-ele">
 
                     <?php echo get_field('content_part_3'); ?>
 
@@ -80,7 +80,7 @@ get_header();
                 </div>
 
 
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12 text-start mt-lg-5 mt-md-0 mt-sm-0 mt-0 fadeinleft-ele">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12 text-start mt-lg-5 mt-md-0 mt-sm-0 mt-0 fadeleft-ele">
 
                     <?php echo get_field('content_part_4'); ?>
 
@@ -97,7 +97,7 @@ get_header();
                     </div> -->
                 </div>
 
-                <div class="col-lg-6 col-md-12 col-sm-12 col-12 position-relative mt-5 fadeinright-ele">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12 position-relative mt-5 faderight-ele">
 
                     <?php echo get_field('content_part_5'); ?>
 
