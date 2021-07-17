97231b28392765c59d2f58d66d9c3936c9186e4c
kaiyu li <krisfk@gmail.com>
2021-07-18 03:57:33 +0800

    .

--- wp-content/themes/unionus/page-company-private-tax.php
+++ wp-content/themes/unionus/page-company-private-tax.php
@@ -224,7 +224,7 @@ get_header();
 
         <div class="row gx-5">
 
-            <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative gold-table-div-wrapper-col">
+            <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative gold-table-div-wrapper-col fadeleft-ele">
                 <div class="gold-table-div-wrapper">
 
                     <img class="table-head-th-img"
@@ -268,7 +268,7 @@ get_header();
 
                 </div>
             </div>
-            <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative">
+            <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative faderight-ele">
                 <div class="gold-table-div-wrapper">
 
                     <img class="table-head-th-img"
