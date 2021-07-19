34b96880d27585c3ea7ab699ab5a44ace3b42b47
kaiyu li <krisfk@gmail.com>
2021-07-20 06:19:29 +0800

    .

--- wp-content/themes/unionus/footer.php
+++ wp-content/themes/unionus/footer.php
@@ -21,15 +21,37 @@
 
 <footer id="colophon" class="site-footer pt-const2" role="contentinfo">
 
-    <?php
-    // echo $main_menu;
-        // $main_menu = wp_get_menu_array('main menu');
 
-// foreach ($main_menu as $menu_item) {
-//     echo 1;
-// }
-        // $main_menu = wp_get_menu_array('main menu');
-?>
+    <ul class="top-menu">
+
+        <li><a class="level-1 our-team " href="https://unionus.com.hk/our-team/">Unionus團隊</a></li>
+        <li><a class="level-1 advantage " href="https://unionus.com.hk/advantage/">優勢</a></li>
+        <li><a class="level-1 parent service-btn" href="#">服務</a>
+            <ul class="mobile-menu-submenu">
+                <li><a class="sme" href="https://unionus.com.hk/sme/">中小企融資擔保計劃 百分百擔保特惠貸款</a></li>
+                <li><a class="drp" href="https://unionus.com.hk/drp/">DRP債務重組</a></li>
+                <li><a class="iva" href="https://unionus.com.hk/iva/">IVA 個人自願安排</a></li>
+                <li><a class="idrp" href="https://unionus.com.hk/idrp/">IDRP 牽頭式債務舒緩</a></li>
+                <li><a class="bankrupt" href="https://unionus.com.hk/bankrupt/">破產</a></li>
+                <li><a class="improve-tu-plan" href="https://unionus.com.hk/improve-tu-plan/">TU（個人環聯信貸報告）改善方案</a></li>
+                <li><a class="large-clearing-scheme" href="https://unionus.com.hk/large-clearing-scheme/">大額清數方案</a>
+                </li>
+                <li><a class="found-limited-unlimited-company"
+                        href="https://unionus.com.hk/found-limited-unlimited-company/">開立有限/無限公司</a></li>
+                <li><a class="company-private-tax" href="https://unionus.com.hk/company-private-tax/">公司/私人報稅</a></li>
+                <li><a class="account-audit" href="https://unionus.com.hk/account-audit/">會計審核</a></li>
+            </ul>
+        </li>
+        <li><a class="level-1 column " href="https://unionus.com.hk/column/">專欄</a></li>
+        <li><a class="level-1 artists-and-partners " href="https://unionus.com.hk/artists-and-partners/">藝人與合作伙伴</a>
+        </li>
+        <li><a class="level-1 contact-us contact-us-btn" href="https://unionus.com.hk/contact-us/">聯絡我們</a></li>
+        <li>
+            <div class="lang-div"> <a href="https://unionus.com.hk/home/">繁</a><span class="sep animate__animated"
+                    style="width: 100%; opacity: 1;">|</span> <a href="https://unionus.com.hk/en/home/">Eng</a> </div>
+        </li>
+    </ul>
+
 
 
 
