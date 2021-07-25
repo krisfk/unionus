7361f91779c9947e5112b50d344e5186c93f04f4
kaiyu li <krisfk@gmail.com>
2021-07-26 04:31:43 +0800

    .

--- wp-content/themes/unionus/page-home.php
+++ wp-content/themes/unionus/page-home.php
@@ -524,7 +524,7 @@ $(function() {
         if ($(window).width > 1280) {
             offset_pos_y = 0
         } else {
-            offset_pos_y = -80
+            offset_pos_y = -100
 
         }
 
@@ -562,7 +562,7 @@ $(function() {
         if ($(window).width > 1280) {
             to_pos_y = $(goToSection).offset().top
         } else {
-            to_pos_y = $(goToSection).offset().top - 80
+            to_pos_y = $(goToSection).offset().top - 100
 
         }
 
