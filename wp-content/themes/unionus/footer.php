3c59f4247809cbf3e090bd9faec2f858098ae7bf
kaiyu li <krisfk@gmail.com>
2021-07-26 16:36:08 +0800

    .

--- wp-content/themes/unionus/footer.php
+++ wp-content/themes/unionus/footer.php
@@ -31,6 +31,18 @@
   if($lang_code=='zh-hant')
   {
         ?>
+        <select name="ask-service" class="wpcf7-form-control wpcf7-select form-control" aria-invalid="false">
+            <option value=""></option>
+            <option value="DRP債務重組">DRP債務重組</option>
+            <option value="IDRP 牽頭式債務舒緩">IDRP 牽頭式債務舒緩</option>
+            <option value="破產">破產</option>
+            <option value="TU（個人環聯信貸報告）改善方案">TU（個人環聯信貸報告）改善方案</option>
+            <option value="大額清數方案">大額清數方案</option>
+            <option value="開立有限/無限公司">開立有限/無限公司</option>
+            <option value="公司/私人報稅">公司/私人報稅</option>
+            <option value="會計審核">會計審核</option>
+        </select>
+
         <ul class="footer-menu">
 
             <li><a class="level-1 our-team " href="https://unionus.com.hk/our-team/">Unionus團隊</a></li>
