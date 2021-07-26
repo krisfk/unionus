f3b252c969be6503cea7e7b00b3759051036f6d9
kaiyu li <krisfk@gmail.com>
2021-07-27 04:41:21 +0800

    .

--- wp-content/themes/unionus/footer.php
+++ wp-content/themes/unionus/footer.php
@@ -32,9 +32,9 @@
   {
         ?>
         <select name="" class="footer-pulldown-menu wpcf7-form-control wpcf7-select form-control" aria-invalid="false">
-            <option value="">其他頁面</option>
-            <option value="https://unionus.com.hk/our-team/">Unionus團隊</option>
-            <option value="https://unionus.com.hk/advantage/">優勢</option>
+            <option value="">服務項目</option>
+            <!-- <option value="https://unionus.com.hk/our-team/">Unionus團隊</option> -->
+            <!-- <option value="https://unionus.com.hk/advantage/">優勢</option> -->
             <option value="https://unionus.com.hk/sme/">中小企融資擔保計劃 百分百擔保特惠貸款</option>
             <option value="https://unionus.com.hk/drp/">DRP債務重組</option>
             <option value="https://unionus.com.hk/iva/">IVA 個人自願安排</option>
@@ -45,9 +45,9 @@
             <option value="https://unionus.com.hk/found-limited-unlimited-company/">開立有限/無限公司</option>
             <option value="https://unionus.com.hk/company-private-tax/">公司/私人報稅</option>
             <option value="https://unionus.com.hk/account-audit/">會計審核</option>
-            <option value="https://unionus.com.hk/column/">專欄</option>
-            <option value="https://unionus.com.hk/artists-and-partners/">藝人與合作伙伴</option>
-            <option value="https://unionus.com.hk/contact-us/">聯絡我們</option>
+            <!-- <option value="https://unionus.com.hk/column/">專欄</option> -->
+            <!-- <option value="https://unionus.com.hk/artists-and-partners/">藝人與合作伙伴</option> -->
+            <!-- <option value="https://unionus.com.hk/contact-us/">聯絡我們</option> -->
 
         </select>
 
@@ -55,18 +55,18 @@
 
             <li><a class="level-1 our-team " href="https://unionus.com.hk/our-team/">Unionus團隊</a></li>
             <li><a class="level-1 advantage " href="https://unionus.com.hk/advantage/">優勢</a></li>
-            <li><a class="sme" href="https://unionus.com.hk/sme/">中小企融資擔保計劃 百分百擔保特惠貸款</a></li>
-            <li><a class="drp" href="https://unionus.com.hk/drp/">DRP債務重組</a></li>
-            <li><a class="iva" href="https://unionus.com.hk/iva/">IVA 個人自願安排</a></li>
-            <li><a class="idrp" href="https://unionus.com.hk/idrp/">IDRP 牽頭式債務舒緩</a></li>
-            <li><a class="bankrupt" href="https://unionus.com.hk/bankrupt/">破產</a></li>
-            <li><a class="improve-tu-plan" href="https://unionus.com.hk/improve-tu-plan/">TU（個人環聯信貸報告）改善方案</a></li>
-            <li><a class="large-clearing-scheme" href="https://unionus.com.hk/large-clearing-scheme/">大額清數方案</a>
-            </li>
-            <li><a class="found-limited-unlimited-company"
-                    href="https://unionus.com.hk/found-limited-unlimited-company/">開立有限/無限公司</a></li>
-            <li><a class="company-private-tax" href="https://unionus.com.hk/company-private-tax/">公司/私人報稅</a></li>
-            <li><a class="account-audit" href="https://unionus.com.hk/account-audit/">會計審核</a></li>
+            <!-- <li><a class="sme" href="https://unionus.com.hk/sme/">中小企融資擔保計劃 百分百擔保特惠貸款</a></li> -->
+            <!-- <li><a class="drp" href="https://unionus.com.hk/drp/">DRP債務重組</a></li> -->
+            <!-- <li><a class="iva" href="https://unionus.com.hk/iva/">IVA 個人自願安排</a></li> -->
+            <!-- <li><a class="idrp" href="https://unionus.com.hk/idrp/">IDRP 牽頭式債務舒緩</a></li> -->
+            <!-- <li><a class="bankrupt" href="https://unionus.com.hk/bankrupt/">破產</a></li> -->
+            <!-- <li><a class="improve-tu-plan" href="https://unionus.com.hk/improve-tu-plan/">TU（個人環聯信貸報告）改善方案</a></li> -->
+            <!-- <li><a class="large-clearing-scheme" href="https://unionus.com.hk/large-clearing-scheme/">大額清數方案</a> -->
+            <!-- </li> -->
+            <!-- <li><a class="found-limited-unlimited-company" -->
+            <!-- href="https://unionus.com.hk/found-limited-unlimited-company/">開立有限/無限公司</a></li> -->
+            <!-- <li><a class="company-private-tax" href="https://unionus.com.hk/company-private-tax/">公司/私人報稅</a></li> -->
+            <!-- <li><a class="account-audit" href="https://unionus.com.hk/account-audit/">會計審核</a></li> -->
             <li><a class="level-1 column " href="https://unionus.com.hk/column/">專欄</a></li>
             <li><a class="level-1 artists-and-partners " href="https://unionus.com.hk/artists-and-partners/">藝人與合作伙伴</a>
             </li>
