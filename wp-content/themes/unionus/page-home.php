209597901ebb529b3941d9e1f5d0fee12fa9936e
kaiyu li <krisfk@gmail.com>
2021-08-12 01:56:57 +0800

    .

--- wp-content/themes/unionus/page-home.php
+++ wp-content/themes/unionus/page-home.php
@@ -20,7 +20,7 @@ get_header();
 <img class="bg-shape-3" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-shape-3.png" alt="">
 <img class="bg-shape-4" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-shape-4.png" alt="">
 
-<img class="white-light-bg white-light-bg-1" data-400="opacity:0" data-1000="opacity:0.1"
+<img class="white-light-bg white-light-bg-1" data-400="opacity:0" data-1200="opacity:0.2"
     src="<?php echo get_template_directory_uri(); ?>/assets/images/white-light-bg-1.png" alt="">
 
 <img class="white-light-bg white-light-bg-2"
