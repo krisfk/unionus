148278d1380586f76ba0993e2b4806407ad8b641
kaiyu li <krisfk@gmail.com>
2022-08-24 05:40:39 +0800

    .

--- wp-content/themes/unionus/assets/js/common.js
+++ wp-content/themes/unionus/assets/js/common.js
@@ -103,7 +103,7 @@ $(function() {
         ) {
             $('.iva-benefits-animation-div').addClass('animate__animated');
 
-            alert(4);
+            $($('.iva-benefits-animation-div .layer-1')).fadeIn(200);
         }
 
         for (i = 0; i < $('.fadeleft-ele').length; i++) {
